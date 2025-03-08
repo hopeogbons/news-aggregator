97a26b664e5a3d5d0f9a779fbaa09e254d169578
Hope Ogbons <hopeogbons@gmail.com>
2025-03-09 00:59:40 +0100

    Mount the redux store globally as the single source of truth

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,16 @@ import { createRoot } from "react-dom/client";
 import { ThemeProvider, CssBaseline } from "@mui/material";
 import { theme } from "./styles/theme.ts";
 import App from "./App.tsx";
+import { Provider } from "react-redux";
+import store from "./redux/store.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </Provider>
   </StrictMode>
 );
