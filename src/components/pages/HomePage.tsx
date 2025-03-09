80708e42a1c0476ca91308bc13f60c41d4c1c965
Hope Ogbons <hopeogbons@gmail.com>
2025-03-09 16:40:29 +0100

    Replace dummy data for categories and authors with real data: - bootstrap fetchCategories - bootstrap fetchAuthors

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -1,53 +1,57 @@
-import { useLayoutEffect, useState } from "react";
+import { useEffect, useLayoutEffect, useState } from "react";
 import {
   Box,
   Checkbox,
   Chip,
   FormControlLabel,
   FormGroup,
+  Skeleton,
   Stack,
 } from "@mui/material";
 import HomeLayout from "../layouts/HomeLayout";
 import FieldsetWithLegend from "../form/FieldsetWithLegend";
 import MultipleSelectChip from "../form/MultipleSelectChip";
 import { fetchCategories } from "../../redux/slices/categoriesSlice";
+import { fetchAuthors } from "../../redux/slices/authorsSlice";
 import { useAppDispatch, useAppSelector } from "../../redux/hooks";
+import styled from "styled-components";
+
+const ChipSkeleton = styled(Skeleton)({
+  borderRadius: "15px",
+});
 
 const HomePage = () => {
   const dispatch = useAppDispatch();
-  const { categories } = useAppSelector((state) => state.categories);
+  const {
+    categories,
+    loading: categoriesLoading,
+    error: categoriesError,
+  } = useAppSelector((state) => state.categories);
+  const {
+    authors,
+    loading: authorsLoading,
+    error: authorsError,
+  } = useAppSelector((state) => state.authors);
 
   useLayoutEffect(() => {
     dispatch(fetchCategories());
+    dispatch(fetchAuthors());
   }, [dispatch]);
 
-  const [selectedCategories, setSelectedCategories] = useState<string[]>([
-    "books",
-    "climate",
-    "education",
-    "entertainment",
-    "health",
-    "politics",
-    "science",
-    "sports",
-    "technology",
-    "travel",
-    "weather",
-    "world",
-  ]);
-  const allAuthors = [
-    "Oliver Hansen",
-    "Van Henry",
-    "April Tucker",
-    "Ralph Hubbard",
-    "Omar Alexander",
-    "Carlos Abbott",
-    "Miriam Wagner",
-    "Bradley Wilkerson",
-    "Virginia Andrews",
-    "Kelly Snyder",
-  ];
+  const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
   const [selectedAuthors, setSelectedAuthors] = useState<string[]>([]);
+
+  useEffect(() => {
+    if (!categoriesLoading && !categoriesError && categories.length > 0) {
+      setSelectedCategories(categories.slice(0, 10));
+    }
+  }, [categoriesLoading, categoriesError, categories]);
+
+  useEffect(() => {
+    if (!authorsLoading && !authorsError && categories.length > 0) {
+      setSelectedAuthors(authors.slice(0, 10));
+    }
+  }, [authorsLoading, authorsError, authors]);
 
   return (
     <HomeLayout>
@@ -90,18 +94,164 @@ const HomePage = () => {
             </FormGroup>
           </FieldsetWithLegend>
           <FieldsetWithLegend title="Categories:">
-            <MultipleSelectChip
-              options={categories}
-              selectedValues={selectedCategories}
-              onChange={setSelectedCategories}
-            />
+            {categoriesLoading ? (
+              <Box
+                sx={{
+                  display: "flex",
+                  alignContent: "space-between",
+                  flexWrap: "wrap",
+                  gap: "5px",
+                  p: "0.8em 0.6em",
+                }}
+              >
+                <ChipSkeleton
+                  variant="rounded"
+                  animation="wave"
+                  height={30}
+                  width={100}
+                />
+                <ChipSkeleton
+                  variant="rounded"
+                  animation="wave"
+                  height={30}
+                  width={40}
+                />
+                <ChipSkeleton
+                  variant="rounded"
+                  animation="wave"
+                  height={30}
+                  width={110}
+                />
+                <ChipSkeleton
+                  variant="rounded"
+                  animation="wave"
+                  height={30}
+                  width={60}
+                />
+                <ChipSkeleton
+                  variant="rounded"
+                  animation="wave"
+                  height={30}
+                  width={90}
+                />
+                <ChipSkeleton
+                  variant="rounded"
+                  animation="wave"
+                  height={30}
+                  width={90}
+                />
+                <ChipSkeleton
+                  variant="rounded"
+                  animation="wave"
+                  height={30}
+                  width={60}
+                />
+                <ChipSkeleton
+                  variant="rounded"
+                  animation="wave"
+                  height={30}
+                  width={80}
+                />
+                <ChipSkeleton
+                  variant="rounded"
+                  animation="wave"
+                  height={30}
+                  width={70}
+                />
+                <ChipSkeleton
+                  variant="rounded"
+                  animation="wave"
+                  height={30}
+                  width={110}
+                />
+              </Box>
+            ) : (
+              <MultipleSelectChip
+                options={categories}
+                selectedValues={selectedCategories}
+                onChange={setSelectedCategories}
+              />
+            )}
           </FieldsetWithLegend>
           <FieldsetWithLegend title="Authors:">
-            <MultipleSelectChip
-              options={allAuthors}
-              selectedValues={selectedAuthors}
-              onChange={setSelectedAuthors}
-            />
+            {authorsLoading ? (
+              <Box
+                sx={{
+                  display: "flex",
+                  alignContent: "space-between",
+                  flexWrap: "wrap",
+                  gap: "5px",
+                  p: "0.8em 0.6em",
+                }}
+              >
+                <ChipSkeleton
+                  variant="rounded"
+                  animation="wave"
+                  height={30}
+                  width={90}
+                />
+                <ChipSkeleton
+                  variant="rounded"
+                  animation="wave"
+                  height={30}
+                  width={90}
+                />
+                <ChipSkeleton
+                  variant="rounded"
+                  animation="wave"
+                  height={30}
+                  width={100}
+                />
+                <ChipSkeleton
+                  variant="rounded"
+                  animation="wave"
+                  height={30}
+                  width={90}
+                />
+                <ChipSkeleton
+                  variant="rounded"
+                  animation="wave"
+                  height={30}
+                  width={130}
+                />
+                <ChipSkeleton
+                  variant="rounded"
+                  animation="wave"
+                  height={30}
+                  width={110}
+                />
+                <ChipSkeleton
+                  variant="rounded"
+                  animation="wave"
+                  height={30}
+                  width={130}
+                />
+                <ChipSkeleton
+                  variant="rounded"
+                  animation="wave"
+                  height={30}
+                  width={140}
+                />
+                <ChipSkeleton
+                  variant="rounded"
+                  animation="wave"
+                  height={30}
+                  width={90}
+                />
+                <ChipSkeleton
+                  variant="rounded"
+                  animation="wave"
+                  height={30}
+                  width={110}
+                />
+              </Box>
+            ) : (
+              <MultipleSelectChip
+                options={authors}
+                selectedValues={selectedAuthors}
+                onChange={setSelectedAuthors}
+              />
+            )}
           </FieldsetWithLegend>
         </Box>
         <Box
