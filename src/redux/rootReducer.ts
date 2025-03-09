a53700c0e1f100e1ae895c22a7f50160d895c12e
Hope Ogbons <hopeogbons@gmail.com>
2025-03-09 16:31:51 +0100

    Mount the author reducer on the root reducer

--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import categoriesReducer from "./slices/categoriesSlice";
+import authorsReducer from "./slices/authorsSlice";
 
 const rootReducer = combineReducers({
   categories: categoriesReducer,
+  authors: authorsReducer,
 });
 
 export default rootReducer;
