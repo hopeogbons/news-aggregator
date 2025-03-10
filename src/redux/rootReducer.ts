3dfdd956af668841393ef6285dd9ca8202eb7690
Hope Ogbons <hopeogbons@gmail.com>
2025-03-10 10:51:00 +0100

    Mount keyword reducer on the root reducer

--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import categoriesReducer from "./slices/categoriesSlice";
 import authorsReducer from "./slices/authorsSlice";
+import keywordsReducer from "./slices/keywordsSlice";
 
 const rootReducer = combineReducers({
   categories: categoriesReducer,
   authors: authorsReducer,
+  keywords: keywordsReducer,
 });
 
 export default rootReducer;
