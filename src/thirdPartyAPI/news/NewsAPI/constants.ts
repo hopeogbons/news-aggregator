7cc3971dc5ed9b7e658314a10370f1bd4c746038
Hope Ogbons <hopeogbons@gmail.com>
2025-03-14 23:30:28 +0100

    Change the value for NEWSAPI_EVERYTHING_V2

--- src/thirdPartyAPI/news/NewsAPI/constants.ts
+++ src/thirdPartyAPI/news/NewsAPI/constants.ts
@@ -1,4 +1,4 @@
 export const API_KEY = import.meta.env.VITE_NEWSAPI_KEY;
 
 export const NEWSAPI_SOURCES_V2 = "https://newsapi.org/v2/sources";
-export const NEWSAPI_EVERYTHING_V2 = "https://newsapi.org/v2/sources";
+export const NEWSAPI_EVERYTHING_V2 = "https://newsapi.org/v2/everything";
