ed1486dcaa6638a1e065615124d698230c411985
Hope Ogbons <hopeogbons@gmail.com>
2025-03-17 23:08:57 +0100

    Remove hardcoded news categories

--- src/thirdPartyAPI/news/NewYorkTimes/constants.ts
+++ src/thirdPartyAPI/news/NewYorkTimes/constants.ts
@@ -1,4 +1,17 @@
-export const API_KEY = import.meta.env.VITE_NEWYORKTIMES_KEY;
+export const API_KEY: string = import.meta.env.VITE_NEWYORKTIMES_KEY;
 
-export const NEWYORKTIMES_SEARCH_V2 =
+export const NEWYORKTIMES_SEARCH_V2: string =
   "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+
+export const NEWYORKTIMES_SECTION_V3: string =
+  "https://api.nytimes.com/svc/news/v3/content/section-list.json";
+
+export const newsApiCategoriesMap: Record<string, string> = {
+  business: "Business",
+  entertainment: "Entertainment",
+  general: "General",
+  health: "Health",
+  science: "Science",
+  sports: "Sports",
+  technology: "Technology",
+};
--- src/thirdPartyAPI/news/TheGuardian/constants.ts
+++ src/thirdPartyAPI/news/TheGuardian/constants.ts
@@ -1,44 +1,8 @@
-export const API_KEY = import.meta.env.VITE_THEGUARDIAN_KEY;
+export const API_KEY: string = import.meta.env.VITE_THEGUARDIAN_KEY;
 
-export const THEGUARDIAN_SEARCH_V2 = "https://content.guardianapis.com/search";
-export const THEGUARDIAN_TAGS_V2 = "https://content.guardianapis.com/tags";
+export const THEGUARDIAN_SEARCH_V2: string =
+  "https://content.guardianapis.com/search";
+export const THEGUARDIAN_TAGS_V2: string =
+  "https://content.guardianapis.com/tags";
 export const THEGUARDIAN_SECTIONS_V2 =
   "https://content.guardianapis.com/sections";
-
-/**
- * To ensure relevance, we are manually curating news-related categories,
- * as The Guardian API encompasses a wide array of categories,
- * many of which fall outside the scope of news-related content.
- */
-export const PREFERRED_CATEGORIES: string[] = [
-  "news",
-  "politics",
-  "sport",
-  "business",
-  "technology",
-  "environment",
-  "science",
-  "culture",
-  "artanddesign",
-  "film",
-  "music",
-  "books",
-  "tv-and-radio",
-  "global-development",
-  "world",
-  "uk-news",
-  "us-news",
-  "australia-news",
-  "education",
-  "society",
-  "law",
-  "media",
-  "fashion",
-  "lifeandstyle",
-  "travel",
-  "money",
-  "healthcare-network",
-  "inequality",
-  "cities",
-  "weather",
-];
