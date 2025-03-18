f8bd589954d25045d1ebde0e1b5802bee34f2c65
Hope Ogbons <hopeogbons@gmail.com>
2025-03-18 10:52:07 +0100

    Create a keyword search component

---
+++ src/components/form/KeywordsSearch.tsx
@@ -0,0 +1,31 @@
+import Paper from "@mui/material/Paper";
+import InputBase from "@mui/material/InputBase";
+import IconButton from "@mui/material/IconButton";
+import SearchIcon from "@mui/icons-material/Search";
+
+const KeywordsSearch = () => {
+  return (
+    <Paper
+      component="form"
+      sx={{
+        p: "2px 4px",
+        display: "flex",
+        alignItems: "center",
+        width: "10em",
+        mt: { xs: 2, md: 0 },
+      }}
+    >
+      <InputBase
+        sx={{ ml: 1, flex: 1 }}
+        size="small"
+        placeholder="Search"
+        inputProps={{ "aria-label": "search" }}
+      />
+      <IconButton type="button" sx={{ p: "10px" }} aria-label="search">
+        <SearchIcon />
+      </IconButton>
+    </Paper>
+  );
+};
+
+export default KeywordsSearch;
